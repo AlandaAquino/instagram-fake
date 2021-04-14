0438d217d70f467a0cfd49c93765cb208a5171a8
AlandaAquino <alandaoaquino@fac.pe.senac.br>
2021-04-14 11:19:12 -0300

    Commit inicial

---
+++ Controllers/usuariosController.js
@@ -0,0 +1,31 @@
+const { Usuario, sequelize } = require('../models/');
+
+const usuariosController = {
+    index: async (req, res) => {
+        let usuarios =  await Usuario.findAll();
+        return res.json(usuarios);
+    },
+    create: async (req, res) => {
+        const {nome, email, senha} = req.body;
+        const usuario = {nome, email, senha};
+        let usuarios = await Usuario.create(usuario)
+        return res.json(usuarios);
+    },
+    update: async (req, res) => {
+        const {id} = req.params;
+        const {nome, email, senha} = req.body;
+        let usuarios = await Usuario.update({nome, email, senha}, {
+            where: {id}
+        });
+        return res.send(usuarios);
+    },
+    delete: async (req, res) => {
+        const {id} = req.params;        
+        let usuarios = await Usuario.destroy({
+            where: {id}
+        });
+        return res.json(usuarios);
+    }
+}
+
+module.exports = usuariosController;
